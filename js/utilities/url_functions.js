c58a057a660cfb66ed13e0dc08b735de613111d1
Jaime Andres <andresdavila6@gmail.com>
2016-01-11 16:45:06 -0300

    updates juicer api url and query param name

--- js/utilities/url_functions.js
+++ js/utilities/url_functions.js
@@ -14,12 +14,12 @@ function juicerQueryParams(countryName, apiKey) {
   return ["recent_first=yes",
           "content_format[]=TextualFormat",
           "like-text=" + encodeURIComponent(countryName),
-          "apikey=" + encodeURIComponent(apiKey)];
+          "api_key=" + encodeURIComponent(apiKey)];
 }
 
 function juicerUrl(countryName) {
   var apiKey = "9OHbOpZpVh9tQZBDjwTlTmsCF2Ce0yGQ";
-  var juicerApiHost = "http://data.test.bbc.co.uk/bbcrd-juicer";
+  var juicerApiHost = "http://juicer.api.bbci.co.uk";
   var queryParams = juicerQueryParams(countryName, apiKey).concat(whiteListedSources()).join('&')
 
   return juicerApiHost + "/articles?" + queryParams;
